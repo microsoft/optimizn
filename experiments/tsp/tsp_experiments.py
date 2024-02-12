7f47e3b1cafc5b22b0ded79beb992719e791dda8
Akshay Sathiya <akshay.sathiya@gmail.com>
2024-02-12 15:19:17 -0800

    updated experiments code

--- experiments/tsp/tsp_experiments.py
+++ experiments/tsp/tsp_experiments.py
@@ -8,34 +8,27 @@ from optimizn.combinatorial.algorithms.traveling_salesman.sim_anneal_tsp\
 from optimizn.combinatorial.algorithms.traveling_salesman.bnb_tsp\
     import TravelingSalesmanProblem
 from python_tsp.heuristics import solve_tsp_simulated_annealing
-from experiments.tsp.tsp_experiments_utils import run_python_tsp_bnb
 from optimizn.combinatorial.opt_problem import load_latest_pckl
 import time
 import shutil
 import os
-import multiprocessing
 
 
-def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
-                        reset_p=1/10000):
-    # specify maximum number of iterations
-    MAX_ITERS = int(1e20)  # very high bound on iterations
-    # since algorithms should use up all compute time
+# specify maximum number of iterations
+MAX_ITERS = int(1e20)  # very high bound on iterations
+# since algorithms should use up all compute time
 
-    # specify number of iterations for logging
-    LOG_ITERS = int(1e7)
+# specify number of iterations for logging (optimizn)
+LOG_ITERS = int(1e7)
 
-    # for collecting results
-    results = dict()
 
-    # create traveling salesman problem parameters
-    city_graph = CityGraph(num_cities)
+def _clear_cont_train_data(opt_prob_obj):
+    # clear continuous training data from previous runs
+    if os.path.isdir(f'Data/{opt_prob_obj.name}'):
+        shutil.rmtree(path=f'Data/{opt_prob_obj.name}')
 
-    # delete continuous training results from previous experiment runs
-    if os.path.isdir('Data/'):
-        shutil.rmtree('Data/')
 
-    # run simulated annealing 1
+def run_sa1(city_graph, results, compute_time_mins=1, num_trials=3, reset_p=0):
     results['sa1'] = []
     results['sa1_time'] = []
     tsp_sa1 = TravSalsmn(city_graph, temp_reduce_factor=0.99)
@@ -49,10 +42,12 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
     results['sa1'].append(tsp_sa1.best_cost)
     results['sa1_time'].append(e - s)
 
-    # run simulated annealing 2
+
+def run_sa2(city_graph, results, compute_time_mins=1, num_trials=3, reset_p=0):
     results['sa2'] = []
     results['sa2_time'] = []
     tsp_sa2 = TravSalsmn(city_graph, temp_reduce_factor=0.99)
+    _clear_cont_train_data(tsp_sa2)
     results['sa2_init_sol'] = tsp_sa2.best_solution
     results['sa2_init_sol_cost'] = tsp_sa2.best_cost
     s = time.time()
@@ -83,7 +78,8 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
         results['sa2'].append(tsp_sa2.best_cost)
         results['sa2_time'].append(e - s)
 
-    # run simulated annealing 3
+
+def run_sa3(city_graph, results, compute_time_mins=1, num_trials=3):
     results['sa3'] = []
     results['sa3_time'] = []
     opt_permutation = list(range(city_graph.num_cities))
@@ -109,7 +105,8 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
     results['sa3'].append(opt_dist)
     results['sa3_time'].append(e - s)
 
-    # run simulated annealing 4
+
+def run_sa4(city_graph, results, compute_time_mins, num_trials):
     results['sa4'] = []
     results['sa4_time'] = []
     opt_permutation = list(range(city_graph.num_cities))
@@ -148,7 +145,8 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
         results['sa4'].append(opt_dist)
         results['sa4_time'].append(e - s)
 
-    # run modified branch and bound 1
+
+def run_mod_bnb1(city_graph, results, compute_time_mins, num_trials):
     results['mod_bnb1'] = []
     results['mod_bnb1_time'] = []
     mod_bnb1 = TravelingSalesmanProblem({'input_graph': city_graph})
@@ -162,10 +160,12 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
     results['mod_bnb1'].append(mod_bnb1.best_cost)
     results['mod_bnb1_time'].append(e - s)
 
-    # run modified branch and bound 2
+
+def run_mod_bnb2(city_graph, results, compute_time_mins, num_trials):
     results['mod_bnb2'] = []
     results['mod_bnb2_time'] = []
     mod_bnb2 = TravelingSalesmanProblem({'input_graph': city_graph})
+    _clear_cont_train_data(mod_bnb2)
     results['mod_bnb2_init_sol'] = mod_bnb2.best_solution
     results['mod_bnb2_init_sol_cost'] = mod_bnb2.best_cost
     s = time.time()
@@ -197,12 +197,8 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
         results['mod_bnb2'].append(mod_bnb2.best_cost)
         results['mod_bnb2_time'].append(e - s)
 
-    # clear continuous training data from previous branch and bound runs
-    class_name = mod_bnb2.__class__.__name__
-    if os.path.isdir(f'Data/{class_name}'):
-        shutil.rmtree(path=f'Data/{class_name}')
 
-    # run traditional branch and bound 1
+def run_trad_bnb1(city_graph, results, compute_time_mins, num_trials):
     results['trad_bnb1'] = []
     results['trad_bnb1_time'] = []
     trad_bnb1 = TravelingSalesmanProblem({'input_graph': city_graph})
@@ -216,10 +212,12 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
     results['trad_bnb1'].append(trad_bnb1.best_cost)
     results['trad_bnb1_time'].append(e - s)
 
-    # run traditional branch and bound 2
+
+def run_trad_bnb2(city_graph, results, compute_time_mins, num_trials):
     results['trad_bnb2'] = []
     results['trad_bnb2_time'] = []
     trad_bnb2 = TravelingSalesmanProblem({'input_graph': city_graph})
+    _clear_cont_train_data(trad_bnb2)
     results['trad_bnb2_init_sol'] = trad_bnb2.best_solution
     results['trad_bnb2_init_sol_cost'] = trad_bnb2.best_cost
     s = time.time()
@@ -250,33 +248,39 @@ def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
         trad_bnb2.persist()
         results['trad_bnb2'].append(trad_bnb2.best_cost)
         results['trad_bnb2_time'].append(e - s)
-    
-    # run traditional branch and bound 3
-    results['trad_bnb3'] = []
-    results['trad_bnb3_time'] = []
-    init_permutation = list(range(city_graph.num_cities))
-    init_dist = 0
-    for i in range(1, len(init_permutation)):
-        init_dist += city_graph.dists[init_permutation[i], init_permutation[i-1]]
-    init_dist += city_graph.dists[
-        init_permutation[0], init_permutation[len(init_permutation) - 1]]
-    results['trad_bnb3_init_sol'] = init_permutation
-    results['trad_bnb3_init_sol_cost'] = init_dist
-    q = multiprocessing.Queue()
-    p = multiprocessing.Process(
-        target=run_python_tsp_bnb, args=(q, city_graph.dists))
-    s = time.time()
-    p.start()
-    p.join(compute_time_mins * 60 * num_trials)
-    if p.is_alive():
-        p.terminate()
-        e = time.time()
-        results['trad_bnb3'].append(init_dist)
-    else:
-        e = time.time()
-        opt_dist = q.get()
-        results['trad_bnb3'].append(opt_dist)
-    results['trad_bnb3_time'].append(e - s)
+
+
+def run_tsp_experiments(num_cities=50, compute_time_mins=1, num_trials=3,
+                        reset_p=0):
+    # for collecting results
+    results = dict()
+
+    # create traveling salesman problem parameters
+    city_graph = CityGraph(num_cities)
+
+    # run simulated annealing 1
+    run_sa1(city_graph, results, compute_time_mins, num_trials, reset_p)
+
+    # run simulated annealing 2
+    run_sa2(city_graph, results, compute_time_mins, num_trials, reset_p)
+
+    # run simulated annealing 3
+    run_sa3(city_graph, results, compute_time_mins, num_trials)
+
+    # run simulated annealing 4
+    run_sa4(city_graph, results, compute_time_mins, num_trials)
+
+    # run modified branch and bound 1
+    run_mod_bnb1(city_graph, results, compute_time_mins, num_trials)
+
+    # run modified branch and bound 2
+    run_mod_bnb2(city_graph, results, compute_time_mins, num_trials)
+
+    # run traditional branch and bound 1
+    run_trad_bnb1(city_graph, results, compute_time_mins, num_trials)
+
+    # run traditional branch and bound 2
+    run_trad_bnb2(city_graph, results, compute_time_mins, num_trials)
 
     # return results
     return results
