732bbb308c0e67b9ae04c017f2726175f78c0019
Akshay Sathiya <akshay.sathiya@gmail.com>
2024-03-21 13:23:15 -0700

    updated branch and bound code with recursive depth-first optimization

--- optimizn/combinatorial/branch_and_bound.py
+++ optimizn/combinatorial/branch_and_bound.py
@@ -2,30 +2,29 @@
 # Licensed under the MIT License.
 
 import time
-from queue import PriorityQueue
 from optimizn.combinatorial.opt_problem import OptProblem
+import inspect
 
 
 class BnBProblem(OptProblem):
     def __init__(self, params, logger=None):
-        self.queue = PriorityQueue()
+        # initialization
         self.total_iters = 0
         self.total_time_elapsed = 0
-        self.depth_first = False
         super().__init__(params, logger)
+
+        # check initial solution
         if self.init_solution is not None and not self.is_feasible(
                 self.init_solution):
             raise Exception('Initial solution is infeasible: '
                             + f'{self.init_solution}')
-
-        self.sol_count = 1  # breaks ties between solutions with same lower
-        # bound and depth, solutions generated earlier are given priority
-
-        # put root solution onto PriorityQueue
+        
+        # initialize call stack with root solution
         root_sol = self.get_root()
-        self.queue.put((self.lbound(root_sol), 0, self.sol_count, root_sol))
-        # solution tuples consist of four values: lower bound, solution depth,
-        # solution count, solution
+        root_gen = self.branch(root_sol)
+        if not inspect.isgenerator(root_gen):
+            raise Exception('Branch method must return a generator')
+        self.call_stack = [(root_sol, root_gen)]
     
     def get_root(self):
         '''
@@ -74,8 +73,6 @@ class BnBProblem(OptProblem):
         if force or iters == 1 or iters % int(log_iters) == 0:
             self.logger.info(f'Iterations (current run): {iters}')
             self.logger.info(f'Iterations (total): {self.total_iters}')
-            queue = list(self.queue.queue)
-            self.logger.info(f'Queue size: {len(queue)}')
             self.logger.info(
                 f'Time elapsed (current run): {time_elapsed} seconds')
             self.logger.info(
@@ -95,14 +92,14 @@ class BnBProblem(OptProblem):
                 f'Updated best solution cost to: {self.best_cost}')
 
     def solve(self, iters_limit=1e6, log_iters=100, time_limit=3600,
-              bnb_type=0, depth_first=False):
+              bnb_type=0):
         '''
         This branch and bound implementation is based on the following sources.
 
-        This method executes either the traditional (bnb_type=0) or modified
+        This method executes either the traditional (bnb_type=0) or look-ahead
         (bnb_type=1) branch and bound algorithm. In traditional branch and
         bound, partial solutions are not completed and are not evaluated
-        against the current best solution, while in modified branch
+        against the current best solution, while in look-ahead branch
         and bound, they are.
 
         Sources:
@@ -115,95 +112,81 @@ class BnBProblem(OptProblem):
         https://www.youtube.com/watch?v=yV1d-b_NeK8, February 2018. Online;
         accessed 16-December-2022.
         '''
-        # initialization
+
+        # initialization for current run
         start = time.time()
         iters = 0
         time_elapsed = 0
         original_total_time_elapsed = self.total_time_elapsed
+        terminate = False
 
-        # if problem class instance is loaded, queue is saved as list, so
-        # convert back to PriorityQueue
-        if type(self.queue) is not PriorityQueue:
-            queue = PriorityQueue()
-            for item in self.queue:
-                queue.put(item)
-            self.queue = queue
-        
-        # rearrange items in queue if depth first changed
-        if depth_first != self.depth_first:
-            new_queue = PriorityQueue()
-            if depth_first:
-                # if depth first is True, formerly False, then prioritize
-                # solutions by depth instead of lower bound
-                while not self.queue.empty():
-                    lbound, depth, sol_count, sol = self.queue.get()
-                    new_queue.put((depth, lbound, sol_count, sol))
+        # helper function to check termination conditions
+        def _terminate():
+            return iters >= iters_limit or time_elapsed >= time_limit
+
+        # recursive helper function to evaluate a solution
+        def _evaluate(sol, sol_gen):
+            # compare against best solution
+            if self.is_feasible(sol):
+                # if solution is feasible, update best solution and best
+                # solution cost if needed
+                self._update_best_solution(sol)
+                return
             else:
-                # if depth first is False, formerly True, then prioritize
-                # solutions by lower bound instead of depth
-                while not self.queue.empty():
-                    depth, lbound, sol_count, sol = self.queue.get()
-                    new_queue.put((lbound, depth, sol_count, sol))
+                # if algorithm type is 1 (modified branch and bound), then
+                # complete solution and update best solution and best
+                # solution cost if needed
+                if bnb_type == 1:
+                    completed_sol = self.complete_solution(sol)
+                    if self.is_feasible(completed_sol):
+                        self._update_best_solution(completed_sol)
             
-            # update queue and depth first property
-            self.queue = new_queue
-            self.depth_first = depth_first
-
-        # explore solutions
-        while not self.queue.empty() and iters < iters_limit and\
-                time_elapsed < time_limit:
-            # get solution, skip if lower bound is not less than best solution
-            # cost
-            if self.depth_first:
-                depth, lbound, _, curr_sol = self.queue.get()
-            else:
-                lbound, depth, _, curr_sol = self.queue.get()
-            if self.cost_delta(self.best_cost, lbound) <= 0:
-                continue
-
-            # get and process branched solutions
-            next_sols = self.branch(curr_sol)
-            for next_sol in next_sols:
-                # process branched solution
-                if self.is_feasible(next_sol):
-                    # if solution is feasible, update best solution and best
-                    # solution cost if needed
-                    self._update_best_solution(next_sol)
-                else:
-                    # if algorithm type is 1 (modified branch and bound), then
-                    # complete solution and update best solution and best
-                    # solution cost if needed
-                    if bnb_type == 1:
-                        completed_sol = self.complete_solution(next_sol)
-                        if self.is_feasible(completed_sol):
-                            self._update_best_solution(completed_sol)
-
-                    # if lower bound is less than best solution cost, put
-                    # incomplete solution into queue
-                    lbound = self.lbound(next_sol)
-                    if self.cost_delta(self.best_cost, lbound) > 0:
-                        self.sol_count += 1
-                        if self.depth_first:
-                            self.queue.put(
-                                (depth - 1, lbound, self.sol_count, next_sol))
-                        else:
-                            self.queue.put(
-                                (lbound, depth - 1, self.sol_count, next_sol))
-
-            # update iterations count and time elapsed
+            # check if a better solution could be obtained
+            if self.cost_delta(self.best_cost, self.lbound(sol)) >= 0:
+                return
+            
+            # update iterations count and time elapsed, log results
             iters += 1
             self.total_iters += 1
             time_elapsed = time.time() - start
             self.total_time_elapsed = original_total_time_elapsed +\
                 time_elapsed
-            # log best solution, best solution cost, and other info
             self._log_results(iters, log_iters, time_elapsed)
+            
+            # check termination conditions
+            if _terminate():
+                self.logger.info(
+                    'Iterations/time limit reached, terminating algorithm')
+                terminate = True
+                return
 
-        # log results, return best solution and best solution cost
-        self._log_results(iters, log_iters, time_elapsed, force=True)
+            # evaluate solutions obtained by branching on the current solution
+            for next_sol in next(sol_gen):
+                next_sol_gen = self.branch(next_sol)
+                self.call_stack.append((next_sol, next_sol_gen))
+                _evaluate(next_sol, next_sol_gen)
+                # do not remove solution from call stack if evaluation was
+                # terminated early
+                if terminate:
+                    break
+                self.call_stack.pop()
+        
+        # run branch and bound algorithm
+        while len(self.call_stack) > 0:
+            # check termination conditions
+            if _terminate():
+                self.logger.info(
+                    'Iterations/time limit reached, terminating algorithm')
+                break
+
+            # evaluate solution on call stack
+            sol, sol_gen = self.call_stack[-1]
+            _evaluate(sol, sol_gen)
+            # do not remove solution from call stack if evaluation was
+            # terminated early
+            if terminate:
+                break
+            self.call_stack.pop()
+        
+        # return best solution and best solution cost
         return self.best_solution, self.best_cost
-
-    def persist(self):
-        # convert the queue to a list before saving solution
-        self.queue = list(self.queue.queue)
-        super().persist()
